a6697ca069f326dde13532c930ce37c107b3f8b2
Kostiantyn777 <locoprodukten@gmail.com>
2018-11-19 18:51:43 +0100

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
 	  client: 'pg',
 	  connection: {
-	    host : '127.0.0.1',
+	    host : 'postgresql-animate-73228',
 	    user : 'postgres',
 	    password : 'gomer787',
 	    database : 'smartbrain'
